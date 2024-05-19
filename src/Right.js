ac1f614a282381423df2d16e574810a6ecdce559
madeyinka007 <mutairadeyinka@yahoo.com>
2024-05-19 05:35:09 +0100

    Initial Commit

--- src/Right.js
+++ src/Right.js
@@ -2,7 +2,7 @@ function Right(){
     return (
         <div class="col-xl-4 col-lg-4 content-left">
 	            <div class="content-left-wrapper">
-	                <a href="index.html" id="logo"><img src="assets/img/logo.png" alt="" width="45" height="45" /></a>
+	                <a href="/" id="logo"><img src="assets/img/logo.png" alt="" width="45" height="45" /></a>
 	                <div id="social">
 	                    <ul>
 	                        <li><a href="#0"><i class="icon-facebook"></i></a></li>
@@ -15,8 +15,8 @@ function Right(){
 	                    <figure><img src="assets/img/info_graphic_1.svg" alt="" class="img-fluid" width="270" height="270" /></figure>
 	                    <h2>We are Hiring</h2>
 	                    <p>Tation argumentum et usu, dicit viderer evertitur te has. Eu dictas concludaturque usu, facete detracto patrioque an per, lucilius pertinacia eu vel.</p>
-	                    <a href="#0" class="btn_1 rounded yellow">Purchase this template</a>
-	                    <a href="#start" class="btn_1 rounded mobile_btn yellow">Start Now!</a>
+	                    <a href="/" class="btn_1 rounded yellow">Purchase this template</a>
+	                    <a href="/" class="btn_1 rounded mobile_btn yellow">Start Now!</a>
 	                </div>
 	                <div class="copy">© 2022 Potenza</div>
 	            </div>
