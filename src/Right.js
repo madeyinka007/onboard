28fe66bb59bcd9453915dcd38266d5a758edc1ad
madeyinka007 <mutairadeyinka@yahoo.com>
2024-05-19 05:51:29 +0100

    Initial Commit

--- src/Right.js
+++ src/Right.js
@@ -2,23 +2,23 @@ function Right(){
     return (
         <div class="col-xl-4 col-lg-4 content-left">
 	            <div class="content-left-wrapper">
-	                <a href="/" id="logo"><img src="assets/img/logo.png" alt="" width="45" height="45" /></a>
-	                <div id="social">
+	                <a href="/" id="logo"><img src="/assets/img/logo.png" alt="" width="45" height="45" /></a>
+	                {/* <div id="social">
 	                    <ul>
 	                        <li><a href="#0"><i class="icon-facebook"></i></a></li>
 	                        <li><a href="#0"><i class="icon-twitter"></i></a></li>
 	                        <li><a href="#0"><i class="icon-google"></i></a></li>
 	                        <li><a href="#0"><i class="icon-linkedin"></i></a></li>
 	                    </ul>
-	                </div>
-	                <div>
-	                    <figure><img src="assets/img/info_graphic_1.svg" alt="" class="img-fluid" width="270" height="270" /></figure>
+	                </div> */}
+	                {/* <div>
+	                    <figure><img src="/assets/img/info_graphic_1.svg" alt="" class="img-fluid" width="270" height="270" /></figure>
 	                    <h2>We are Hiring</h2>
 	                    <p>Tation argumentum et usu, dicit viderer evertitur te has. Eu dictas concludaturque usu, facete detracto patrioque an per, lucilius pertinacia eu vel.</p>
 	                    <a href="/" class="btn_1 rounded yellow">Purchase this template</a>
 	                    <a href="/" class="btn_1 rounded mobile_btn yellow">Start Now!</a>
 	                </div>
-	                <div class="copy">© 2022 Potenza</div>
+	                <div class="copy">© 2022 Potenza</div> */}
 	            </div>
 	        </div>
     )
